7defdf242719c98f926ce7239cb7fee86897bea1
Mike Caine <mikey.caine@btinternet.com>
2020-10-25 20:18:26 +0000

    security by obscurity

---
+++ src/main/java/cspam/pointer/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package cspam.pointer;
+
+public class InvalidParametersException extends RuntimeException {
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+}
