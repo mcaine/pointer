4cde2e90d2d68f3e8a828d0cbae1e4401a435b2e
Mike Caine <mikey.caine@btinternet.com>
2025-05-06 08:45:09 +0100

    2025 update

--- src/main/java/cspam/pointer/PointerApplication.java
+++ src/main/java/cspam/pointer/PointerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
